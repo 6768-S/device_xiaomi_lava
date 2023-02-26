29d8ebef781b7455b7b8aa4d2822ea350072ea67
Matsvei Niaverau <matvej2002@gmail.com>
2023-02-27 00:06:46 +0100

    lava: overlay: Enable aux cameras for aperture
    Change-Id: If0bd8514092f55409290ca892f804b0a8f961d88

--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/aurisys_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aurisys_config.xml
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
     FrameworksResOverlayLava
 
